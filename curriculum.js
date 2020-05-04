7080f177cfb948ba7d8564bb4c040a8b64916229
Glitch (eliaspapp) <none>
2020-05-04 03:39:58 +0000

    04/05/2020

--- curriculum.js
+++
@@ -1,3 +0,0 @@
-var programs = ["Adobe Animate", "Paint.NET", "Autodesk Maya", "Aseprite", "Construct", "Scratch", "Stencyl", "Sony Vegas", "Unity3D", "C#", "Microsoft Office", "HTML, CSS e JS", "Unreal Blueprints", "Blender", "Python", "Javascript", "Arduino", "C++", "Ruby", "Java", "Illustrator", "Photoshop"];
-
-var cols = 3;
